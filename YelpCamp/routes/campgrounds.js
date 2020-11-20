5e4e8df4c2fe591917792755c23bc714278e60c5
vsutinah <52406232+vsutinah@users.noreply.github.com>
2020-11-20 11:56:36 +1100

    Basic cloudinary and image upload functionality added (still unable to upload to cloudinary)

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -1,13 +1,25 @@
 const express = require('express'),
     catchAsync = require('../utils/catchAsync'),
     campgrounds = require('../controllers/campgrounds'),
+    multer = require('multer'),
+    { storage } = require('../cloudinary/index'),
+    upload = multer({ storage }), // Set upload destination for images to be our cloud storage   
     router = express.Router(),
     { isLoggedIn, isAuthor, validateCampground } = require('../middlewares/middleware');
 
 // Campground Routes
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        try {
+            console.log(req.body, req.files);
+            res.send('IMAGE UPLOADED');
+        } catch (e) {
+            console.log(e);
+        }
+        
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
